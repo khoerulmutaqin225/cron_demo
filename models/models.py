9dae3149e651c0488766605355b1c30ad038a110
khoerulmutaqin225 <khoerulmutaqin225@gmail.com>
2024-02-29 23:16:28 +0800

    Update Model

--- models/models.py
+++ models/models.py
@@ -11,6 +11,49 @@ class test_email(models.Model):
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+    status = fields.Selection([
+        ('cancel', 'Cancel'),
+        ('open', 'Open'),
+        ('close', 'Close'),
+        ('finish', 'Finish'),
+    ], string='Status', default='open') 
+    
+    
+    pic_perusahaan = fields.Char()
+    jenis_po = fields.Char()
+    nama_customer = fields.Char()
+    kode_customer = fields.Char()
+    tanggal_konfirm_user = fields.Char()
+    tanggal_terima_po = fields.Char()
+    name = fields.Char()
+    tanggal_permintaan = fields.Char()
+    requested_by = fields.Char()
+    vessel_divisi = fields.Char()
+    project_unit = fields.Char()
+    kode_budget = fields.Char()
+    prioritas = fields.Char()
+    nama_produk = fields.Char()
+    jenis_tipe = fields.Char()
+    vol = fields.Char()
+    satuan = fields.Char()
+    supply = fields.Char()
+    kode_unit_barang = fields.Char()
+    status_list = fields.Char()
+    nomor_do = fields.Char()
+    tanggal_do = fields.Char()
+    est_date = fields.Char()
+    nama_penerima = fields.Char()
+    remark = fields.Char()
+    date_remark = fields.Char()
+    remark_2 = fields.Char()
+    date_remark_2 = fields.Char()
+    remark_3 = fields.Char()
+    date_remark_3 = fields.Char()
+    remark_4 = fields.Char()
+    date_remark_4 = fields.Char()
+    remark_5 = fields.Char()
+    date_remark_5 = fields.Char()
+
 
     @api.depends('value')
     def _value_pc(self):
@@ -22,6 +65,7 @@ class test_email(models.Model):
     def action_send_email(self):
         data = 'mutaqin'
         
-        mail_template = self.env.ref('test_email.email_template')
-        mail_template.email_to = 'alwanassyauqi11@gmail.com'
+        mail_template = self.env.ref('cron_demo.email_template')
+        mail_template.email_to = 'khoerulmutaqin529@yahoo.com'
+        mail_template.email_from = 'khoerulmutaqin225@gmail.com'
         mail_template.send_mail(self.id, force_send=True)
