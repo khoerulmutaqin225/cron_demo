bf46d1a2a4963e5210ab1e6ed672ab934953cac7
khoerulmutaqin225 <khoerulmutaqin225@gmail.com>
2024-02-29 23:16:20 +0800

    Update Cron Job

--- __manifest__.py
+++ __manifest__.py
@@ -28,6 +28,7 @@
         'views/views.xml',
         'views/templates.xml',
         'views/email_template.xml',
+        'views/cron.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
